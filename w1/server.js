676212943e39cc724c5bccb2e21c6d9681a9d7b0
Johnathan Ruiz <johnathanruiz@gmail.com>
2024-01-20 05:15:03 -0500

    add changes

--- w1/server.js
+++ w1/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const bodyParser =require('body-parser')
+const mongodb = require('./db/connect')
+
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 3300;
 
-app.use("/", require("./routes"))
+  app
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+    })
+  .use('/', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-})
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
