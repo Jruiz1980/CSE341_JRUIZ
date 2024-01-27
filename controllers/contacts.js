85627f5dc9a28c116996e17c092e2ede3e3e7ea3
Jruiz1980 <johnathanruiz@gmail.com>
2024-01-27 04:40:00 -0500

    add changes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -63,13 +63,19 @@ const updateContact = async (req, res) => {
 
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    res.status(502).json(response.error || 'Some error occurred while deleting the contact.');
   }
 };
 
-module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
+module.exports = {
+                  getAll, 
+                  getSingle, 
+                  createContact, 
+                  updateContact, 
+                  deleteContact 
+                };
