edb8ebcb8728d64d54cae446425e3e51389f306d
Jruiz1980 <johnathanruiz@gmail.com>
2024-01-27 21:31:21 -0500

    add format

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,11 +11,7 @@ const getAll = async (req, res, next) => {
 
 const getSingle = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
+  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
@@ -54,7 +50,7 @@ const updateContact = async (req, res) => {
     .collection('contacts')
     .replaceOne({ _id: userId }, contact);
   console.log(response);
-  if (response.modifiedCount > 0) {
+  if (response.modifiedCount > 1) {
     res.status(204).send();
   } else {
     res.status(500).json(response.error || 'Some error occurred while updating the contact.');
@@ -63,19 +59,23 @@ const updateContact = async (req, res) => {
 
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userId }, true);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .deleteOne({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(502).json(response.error || 'Some error occurred while deleting the contact.');
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
 };
 
 module.exports = {
-                  getAll, 
-                  getSingle, 
-                  createContact, 
-                  updateContact, 
-                  deleteContact 
-                };
+  getAll,
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
+};
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const bodyParser =require('body-parser')
-const mongodb = require('./db/connect')
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-  app
-    .use(bodyParser.json())
-    .use((req, res, next) => {
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-    })
+  })
   .use('/', require('./routes'));
-
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
@@ -21,4 +20,4 @@ mongodb.initDb((err, mongodb) => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
-})
+});
