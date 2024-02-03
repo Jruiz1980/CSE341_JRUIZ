f9744db4bbbb01c91d5e99e2a15ca2e2d3524777
Jruiz1980 <johnathanruiz@gmail.com>
2024-02-03 07:12:13 -0500

    add swagger archive

---
+++ swagger.js
@@ -0,0 +1,13 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Contacts API'
+  },
+  host: 'cse341-jiruiz.onrender.com/contacts',
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
